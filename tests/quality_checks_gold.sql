4f835133a2b1a9e5e6486d9a0d3d37bad1b7c57a
tonechoy <tonechoy@hotmail.com>
2025-03-21 22:02:48 +0800

    Create quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,65 @@
+/*
+========================================================================
+Quality Checks
+========================================================================
+Script Purpose:
+  This script performs quality checks to validate the integrity, consistency, and accuracy of the Gold Layer. These checks ensure:
+  - Uniqueness of surrogate keys in dimension tables.
+  - Referential integrity between keys in dimension tables.
+  - Validation of relaitonship in the data model for analytical purposes.
+
+Usage Notes:
+  - Run these checks after data loading Silver Layer.
+  - Investigate and resolve any discrepancies found during the checks.
+========================================================================
+*/
+
+--========================================================================
+--Checking 'gold.dim_customers'
+--========================================================================
+-- GOLD PRODUCTS
+SELECT * 
+FROM gold.dim_products
+
+-- Checking for Uniqueness of Customers Key in gold.dim_customers
+-- Expectation: No results
+SELECT
+customer_key,
+COUNT(*)
+FROM gold.dim_products
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+
+--========================================================================
+--Checking 'gold.dim_products'
+--========================================================================
+-- GOLD PRODUCTS
+SELECT * 
+FROM gold.dim_products
+
+-- Checking for Uniqueness of Customers Key in gold.dim_products
+-- Expectation: No results
+SELECT
+product_key,
+COUNT(*)
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+
+--========================================================================
+--Checking 'gold.fact_sales'
+--========================================================================
+-- GOLD FACT
+SELECT *
+FROM gold.fact_sales
+
+-- Foreign Key Integrity (Dimensions)
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL
